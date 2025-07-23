7c249387f617ce3851bfe8bc846eddef6170c03e
James Curtis <mybastion@gmail.com>
2025-07-23 08:29:46 +0100

    feat(project): Basic layout with elm-ui
    Add elm-ui and apply basic structure to the page.

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,11 @@
 module Main exposing (..)
 
 import Browser exposing (Document)
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Element exposing (..)
+import Html exposing (Html)
 import Html.Events exposing (..)
+import Element.Input exposing (button, labelHidden, multiline, placeholder)
+import Element.Background as Background
 
 
 main =
@@ -22,6 +24,9 @@ main =
 type alias Model =
     { year : Int
     , day : Int
+    , input : String
+    , partOne : Maybe Int
+    , partTwo : Maybe Int
     }
 
 
@@ -29,6 +34,9 @@ init : () -> ( Model, Cmd Msg )
 init _ =
     ( { year = 2024
       , day = 1
+      , input = ""
+      , partOne = Nothing
+      , partTwo = Nothing
       }
     , Cmd.none
     )
@@ -41,6 +49,8 @@ init _ =
 type Msg
     = YearChanged Int
     | DayChanged Int
+    | InputChanged String
+    | SubmitPressed
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -52,6 +62,11 @@ update msg model =
         DayChanged day ->
             ( { model | day = day }, Cmd.none )
 
+        InputChanged input ->
+            ({model | input = input }, Cmd.none)
+
+        SubmitPressed ->
+            (model, Cmd.none)
 
 
 -- SUBSCRIPTIONS
@@ -75,7 +90,44 @@ type alias Document msg =
 view : Model -> Document Msg
 view model =
     { title = "Advent of Code with Elm"
-    , body =
-        [ div [] [ text "Hello World" ]
+    , body = [
+        layout [] (viewPageColumn model)
+    ]
+    }
+
+
+viewPageColumn model =
+    el [centerX, centerY] (column
+        [ width fill
+        , height fill
+        , spacing 20
         ]
+        [ viewPageHeading
+        , multiline 
+        [ width (px 500)
+        , height (px 200)
+        ] 
+            { onChange = InputChanged
+            , text = model.input
+            , placeholder = Just <| placeholder [] <| text "Type your message"
+            , spellcheck = False
+            , label = labelHidden "Input"
+            }
+        , button
+         [ Background.color color.lightBlue
+         , padding 10
+         , width (px 500)
+         ] 
+        { onPress = Just SubmitPressed
+        , label = el [centerX] (text "Submit")
+        }]
+    )
+
+viewPageHeading : Element msg
+viewPageHeading =
+    el [centerX, spacing 10] (text "Advent of Code with Elm")
+
+
+color =
+    { lightBlue = rgb255 0xC5 0xE8 0xF7
     }
